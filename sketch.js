bf3d76bfda0c6218629061dced8e21612c2f4bdb
Benjamin <benjamin_kok@hotmail.com>
2024-02-18 13:43:17 +0800

    v0.0 created base template file

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(20);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
